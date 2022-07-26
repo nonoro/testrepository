a16af93193001f0cb204ff58dfe2a723bb1efa96
권규정 <nonoro@nonoro.local>
2022-07-27 02:31:59 +0900

    feat : 10798변경

--- src/_10798/Main.java
+++ src/_10798/Main.java
@@ -1,40 +1,25 @@
 package _10798;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[][] strings = new String[5][15];
         Scanner sc = new Scanner(System.in);
-        char[][] array = new char[5][15];
 
         for (int i = 0; i < 5; i++) {
-            String string = sc.nextLine();
-            char[] chars = string.toCharArray();
-
-            System.arraycopy(chars, 0, array[i], 0, chars.length);
+            String[] split = sc.nextLine().split("");
+            for (int j = 0; j < split.length; j++) {
+                strings[i][j] = split[j];
+            }
         }
-
-
 
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 5; j++) {
-                char tmp = array[j][i];
-                if (isValidChar(tmp)) {
-                    System.out.print(tmp);
+                if (strings[j][i] != null && j < 5) {
+                    System.out.print(strings[j][i]);
                 }
             }
         }
     }
-
-    private static boolean isValidChar(char tmp) {
-        return (tmp >= 'a' && tmp <= 'z') || (tmp >= '0' && tmp <= '9') || (tmp >= 'A' && tmp <= 'Z');
-    }
 }
-
-
-
-
-
