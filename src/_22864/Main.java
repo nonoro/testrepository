76843def52d0ad8cd636529d505d1647bbb80620
권규정 <nonoro@nonoro.local>
2022-08-04 00:41:50 +0900

    feat : 22864

---
+++ src/_22864/Main.java
@@ -0,0 +1,39 @@
+package _22864;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static final int number = 24;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().split(" ");
+
+
+        int A = Integer.parseInt(s[0]); // 피로도
+        int B = Integer.parseInt(s[1]); // 시간당 처리할 수 있는 일의양
+        int C = Integer.parseInt(s[2]); // 피로도감소
+        int D = Integer.parseInt(s[3]); // 최대 피로도
+        int sum = 0;
+        int count = 0;
+        int aCount = 0;
+
+
+        for (int i = 0; i < 24; i++) {
+            if ((sum + A) > D) {
+                sum -= C;
+                if (sum < 0) {
+                    sum = 0;
+                }
+                continue;
+            }
+
+            aCount += B;
+            sum += A;
+        }
+
+        System.out.println(aCount);
+
+    }
+}
